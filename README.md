985a90cd3b595daa4d7cfc988ffffbbe12d89f4a
Bradley A <138368512+boldybread@users.noreply.github.com>
2024-05-06 10:10:09 +1000

    feat: first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # node-file-stuff
+
+synchronous function calls synchronous function calls asynchronous function calls synchronous function
---
+++ src/index.js
@@ -0,0 +1,121 @@
+
+const fs = require("node:fs");
+const fsPromises = require("node:fs/promises");
+
+
+console.log("Promise output:" + doesFileExistPromise("./pokemonStats.json"));
+console.log("Sync output:" + doesFileExistSync("./pokemonStats.json"));
+
+(async () => {
+	let asyncResult = await doesFileExistAsync("./pokemonStats.json");
+	console.log("Async output:" + asyncResult.size);
+})();
+
+
+/*
+
+async function to get pokemon data 
+
+await for result 
+
+update the screen 
+
+*/
+
+async function doesFileExistAsync(targetPath){
+	return await fsPromises.stat(targetPath);
+
+	// let result = false;
+	// result = await fsPromises.stat(targetPath);
+	// return result;
+}
+
+
+function doesFileExistPromise(targetPath){
+	let result = false;
+
+	return new Promise((resolve, reject) => {
+		fsPromises.stat(targetPath).then(statData => {
+			if (statData.size || statData.birthtimeMs) {
+				result = true;
+				console.log(result);
+				resolve(result)
+			} else {
+				resolve(result)
+			}
+		}).catch(error => {
+			reject(error);
+		});
+
+	});
+
+	
+	// function(data, data, whatever, callbackFunction)
+	// fs.exists(targetPath, doSomethingWithResult());
+	// fs.exists(targetPath, (existsResult) => {
+	// 	result = existsResult;
+	// 	return result;
+	// });
+
+	// fs.exists(targetPath).then((existsResult) => {
+	// 	// if (existsResult){
+	// 	// 	result = true;
+	// 	// } else {
+	// 	// 	result = false;
+	// 	// }
+	// 	result = existsResult;
+	// }).catch((error) => {
+	// 	console.error(error);
+	// });
+
+	// return result;
+}
+
+function doesFileExistSync(targetPath){
+	let result = false;
+	if (fs.existsSync(targetPath)){
+		result = true;
+	} else {
+		result = false;
+	}
+	return result;
+}
+
+
+function createJsonFile(targetPath, data){
+	// TODO: wishlist item because a json file already exists for us
+}
+
+
+async function loadDataFromFile(targetPath){
+	let data = null;
+
+	let doesFileExist = await doesFileExistAsync(targetPath);
+	if (doesFileExist){
+		// data is null
+		data = await fsPromises.readFile(targetPath, { encoding: 'utf8' });
+		// data is a string
+		data = JSON.parse(data);
+		// data is an object 
+		console.log("Pokemon caught is: " + data["pokemonCaught"]);
+	}
+
+	return data;
+}
+
+(async () => {
+
+	let fileData = await loadDataFromFile("./pokemonStats.json");
+	console.log(fileData);
+})()
+
+
+/*
+Technique 2 to modify keys in JSON:
+function editKey(targetKey, newData){}
+*/
+
+
+function saveDataToFile(targetPath, data){
+	// TODO: Write data to JSON file logic goes here
+}
